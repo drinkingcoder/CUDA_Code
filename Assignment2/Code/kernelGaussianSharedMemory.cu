6d52d9e3d6e5924e08f01f3880775cdda7f7bcaa
drinkingcoder <drinkingcoder@gmail.com>
2017-11-13 22:02:18 +0800

    Debuggin the reason of Shared Memory is slower than global memory.

--- Assignment2/Code/kernelGaussianSharedMemory.cu
+++ Assignment2/Code/kernelGaussianSharedMemory.cu
@@ -4,6 +4,10 @@
 
 #include "kernelGaussianSharedMemory.hpp"
 
+__device__ int MapThreadToGlobal(int tx,int ty) {
+    return (blockIdx.x * blockDim.x + tx) + (blockIdx.y * blockDim.y + ty)*IMAGESIZE_WIDTH;
+}
+
 __global__ static void kernel(unsigned char *result, unsigned char *ptr, double *gaussian_kernel) {
     __shared__ unsigned char patch[PATCHSIZE_HEIGHT*PATCHSIZE_WIDTH*3];
     __shared__ double cachedGaussian[KERNEL_SIZE*KERNEL_SIZE];
@@ -171,6 +175,104 @@ __global__ static void kernel2(unsigned char *result, unsigned char *ptr, double
     }
 }
 
+__global__ static void kernel3(unsigned char *result, unsigned char *ptr, double *gaussian_kernel) {
+    __shared__ unsigned char patch[(PATCHSIZE_WIDTH+KERNEL_SIZE-1)*(PATCHSIZE_HEIGHT+KERNEL_SIZE-1)*3];
+    __shared__ double cachedGaussian[KERNEL_SIZE*KERNEL_SIZE];
+
+    int x = blockIdx.x*blockDim.x+threadIdx.x;
+    int y = blockIdx.y*blockDim.y+threadIdx.y;
+    int offset;
+
+//    if( threadIdx.x < KERNEL_SIZE && threadIdx.y < KERNEL_SIZE ) {
+//        int poffset =  ( threadIdx.x + threadIdx.y*KERNEL_SIZE );
+//        cachedGaussian[poffset] = gaussian_kernel[poffset];
+//    }
+
+    int px,py,poffset,tx = x,ty = y;
+    int patchSizeX = KERNEL_SIZE-1 + blockDim.x;
+    bool flag = false;
+
+    if( x < KERNEL_SIZE/2 || x >= IMAGESIZE_WIDTH - KERNEL_SIZE/2 ) return;
+    if( y < KERNEL_SIZE/2 || y >= IMAGESIZE_HEIGHT - KERNEL_SIZE/2 ) return;
+
+//    if( threadIdx.y < KERNEL_SIZE/2 ) {
+        flag = true;
+        tx = threadIdx.x;
+        ty = threadIdx.y - KERNEL_SIZE/2;
+//    } else if(threadIdx.y < KERNEL_SIZE/2 * 2 ) {
+//        flag = true;
+//        tx = threadIdx.y - KERNEL_SIZE/2 * 2;
+//        ty = threadIdx.x;
+//    } else if(threadIdx.y < KERNEL_SIZE/2 * 3) {
+//        flag = true;
+//        tx = threadIdx.y - KERNEL_SIZE/2 * 2 + blockDim.x;
+//        ty = threadIdx.x;
+//    } else if(threadIdx.y < KERNEL_SIZE/2 * 4){
+//        flag = true;
+//        tx = threadIdx.x;
+//        ty = threadIdx.y - KERNEL_SIZE/2 * 3 + blockDim.y;
+//    }
+//    if(flag) {
+//        px = tx + KERNEL_SIZE/2;
+//        py = ty + KERNEL_SIZE/2;
+//        poffset = (py*patchSizeX + px)*3;
+//        offset = MapThreadToGlobal(tx, ty)*3;
+//        patch[poffset + 0]  = ptr[ offset + 0 ];
+//        patch[poffset + 1]  = ptr[ offset + 1 ];
+//        patch[poffset + 2]  = ptr[ offset + 2 ];
+////        flag = false;
+//    }
+//    if(KERNEL_SIZE/2 * 4 <= threadIdx.y && threadIdx.y < KERNEL_SIZE/2 * 5) {
+//        if(threadIdx.x < KERNEL_SIZE/2) {
+//            flag = true;
+//            tx = threadIdx.x - KERNEL_SIZE/2;
+//            ty = threadIdx.y - KERNEL_SIZE/2 * 5;
+//        } else if(threadIdx.x < KERNEL_SIZE/2 * 2) {
+//            flag = true;
+//            tx = threadIdx.x - KERNEL_SIZE/2 * 2;
+//            ty = threadIdx.y - KERNEL_SIZE/2 * 4 + blockDim.y;
+//        } else if(threadIdx.x < KERNEL_SIZE/2 * 3) {
+//            flag = true;
+//            tx = threadIdx.x - KERNEL_SIZE/2 * 2 + blockDim.x;
+//            ty = threadIdx.y - KERNEL_SIZE/2 * 4 + blockDim.y;
+//        } else if(threadIdx.x < KERNEL_SIZE/2 * 4) {
+//            flag = true;
+//            tx = threadIdx.x - KERNEL_SIZE/2 * 3 + blockDim.x;
+//            ty = threadIdx.y - KERNEL_SIZE/2 * 5;
+//        }
+//    }
+//    if(flag) {
+//        px = tx + KERNEL_SIZE/2;
+//        py = ty + KERNEL_SIZE/2;
+//        poffset = (py*patchSizeX + px)*3;
+//        offset = MapThreadToGlobal(tx, ty)*3;
+//        patch[poffset + 0]  = ptr[ offset + 0 ];
+//        patch[poffset + 1]  = ptr[ offset + 1 ];
+//        patch[poffset + 2]  = ptr[ offset + 2 ];
+//    }
+//    py = threadIdx.y + KERNEL_SIZE/2;
+//    px = threadIdx.x + KERNEL_SIZE/2;
+//    poffset = (py*patchSizeX + px)*3;
+//    offset = (x+y*IMAGESIZE_WIDTH)*3;
+//
+//    patch[poffset] = ptr[offset];
+//    patch[poffset+1] = ptr[offset+1];
+//    patch[poffset+2] = ptr[offset+2];
+//
+//    __syncthreads();
+
+    offset = (x+y*IMAGESIZE_WIDTH);
+    poffset = (threadIdx.x + threadIdx.y*patchSizeX);
+    for(int channel = 0; channel<3; channel++)
+    {
+        double tmp_result = 0;
+        for( int i = 0; i < KERNEL_SIZE; i++)
+            for( int j = 0; j < KERNEL_SIZE; j++) {
+                tmp_result += patch[(poffset + i * patchSizeX + j) * 3 + channel] * cachedGaussian[i * KERNEL_SIZE + j];
+            }
+        result[offset*3 + channel] = (unsigned char) (tmp_result);
+    }
+}
 void KernelGaussianSharedMemory::awakeKernel(){
 #define BLOCKSIZE 32
 #define GRIDSIZEX (IMAGESIZE_WIDTH/BLOCKSIZE+1)
@@ -178,5 +280,5 @@ void KernelGaussianSharedMemory::awakeKernel(){
     dim3 gridDim(GRIDSIZEX,GRIDSIZEY);
     dim3 blockDim(BLOCKSIZE,BLOCKSIZE);
 
-    kernel<<<gridDim,blockDim>>>(m_devResultBitmap,m_devInputBitmap,m_devGaussianKernel);
+    kernel3<<<gridDim,blockDim>>>(m_devResultBitmap,m_devInputBitmap,m_devGaussianKernel);
 }
